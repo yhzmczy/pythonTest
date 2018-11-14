82b870bd7f01dca8d6ad9868cb18aac7aee9fc21
yhzmczy <576529339@qq.com>
2018-11-14 11:11:34 +0800

    函数person中添加参数判断

--- 8函数2.py
+++ 8函数2.py
@@ -23,6 +23,12 @@ print(calc2(*l))
 #可变参数允许你传入0个或任意个参数，这些可变参数在函数调用时自动组装为一个tuple。
 #而关键字参数允许你传入0个或任意个含参数名的参数，这些关键字参数在函数内部自动组装为一个dict。
 def person(name, age, **kw):
+    if 'city' in kw:
+        # 有city参数
+        pass
+    if 'job' in kw:
+        # 有job参数
+        pass
     print('name:', name, 'age:', age, 'other:', kw)
 
 person('Michael', 30)
